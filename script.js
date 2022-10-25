fa6ee8a75ec6bb6f2fbbb53e68079f4b73862651
Nabeel Akbar <studiosnabeel@gmail.com>
2022-10-25 17:57:55 +0500

    Add Footer section HTML/CSS

---
+++ script.js
@@ -0,0 +1,9 @@
+const teachersAll = [
+  {
+    teacherImg: '',
+    teacherName: '',
+    teacherExp: 'Software Developer at Microsoft',
+    teacherAbout:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsum eius mollitia? Sit, corrupti libero.',
+  },
+];
