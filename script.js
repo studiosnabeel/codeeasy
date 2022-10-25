231d7a1798430d5fcc1243d5dabaf024140e5781
Nabeel Akbar <studiosnabeel@gmail.com>
2022-10-25 18:28:06 +0500

    Add speaker list dynamically

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
 const teachersAll = [
   {
-    teacherImg: '',
-    teacherName: '',
+    teacherImg: './Teacher pics/andrea.jpg',
+    teacherName: 'Andrea Thompson',
+    teacherExp: 'Software Developer at Microsoft',
+    teacherAbout:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsum eius mollitia? Sit, corrupti libero.',
+  },
+  {
+    teacherImg: './Teacher pics/Antoni.jpg',
+    teacherName: 'Antoni Gozalas',
+    teacherExp: 'Software Developer at Microsoft',
+    teacherAbout:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsum eius mollitia? Sit, corrupti libero.',
+  },
+  {
+    teacherImg: './Teacher pics/Edmon.jpg',
+    teacherName: 'Edmond Dennis',
+    teacherExp: 'Software Developer at Microsoft',
+    teacherAbout:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsum eius mollitia? Sit, corrupti libero.',
+  },
+  {
+    teacherImg: './Teacher pics/Ron.jpg',
+    teacherName: 'Ron Jimmy',
+    teacherExp: 'Software Developer at Microsoft',
+    teacherAbout:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsum eius mollitia? Sit, corrupti libero.',
+  },
+  {
+    teacherImg: './Teacher pics/Vanessa.jpg',
+    teacherName: 'Vanessa Jack',
     teacherExp: 'Software Developer at Microsoft',
     teacherAbout:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsum eius mollitia? Sit, corrupti libero.',
   },
 ];
+
+const speakerBio = document.querySelector('.speakers-cards');
+
+teachersAll.forEach((speakerBio) => {
+  const div = document.createElement('div');
+  div.innerHTML += `
+    <div class="subCard">
+    <img src="${speakerBio.teacherImg}">
+    <div>
+        <h2>${speakerBio.teacherName}</h2>
+        <h3>${speakerBio.teacherExp}</h3>
+        <p>${speakerBio.teacherAbout}</p>
+    </div>
+    </div>
+    `;
+  speakerBio.append(div);
+});
