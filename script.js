7f438b567bdaf7d1f010ca46d981ddd8d04c77dc
Nabeel Akbar <studiosnabeel@gmail.com>
2022-10-25 19:52:16 +0500

    Add style to speaker section

--- script.js
+++ script.js
@@ -3,36 +3,31 @@ const teachersAll = [
     teacherImg: './Teacher pics/andrea.jpg',
     teacherName: 'Andrea Thompson',
     teacherExp: 'Software Developer at Microsoft',
-    teacherAbout:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsum eius mollitia? Sit, corrupti libero.',
+    teacherAbout: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
   {
     teacherImg: './Teacher pics/Antoni.jpg',
     teacherName: 'Antoni Gozalas',
     teacherExp: 'Software Developer at Microsoft',
-    teacherAbout:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsum eius mollitia? Sit, corrupti libero.',
+    teacherAbout: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
   {
     teacherImg: './Teacher pics/Edmon.jpg',
     teacherName: 'Edmond Dennis',
     teacherExp: 'Software Developer at Microsoft',
-    teacherAbout:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsum eius mollitia? Sit, corrupti libero.',
+    teacherAbout: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
   {
     teacherImg: './Teacher pics/Ron.jpg',
     teacherName: 'Ron Jimmy',
     teacherExp: 'Software Developer at Microsoft',
-    teacherAbout:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsum eius mollitia? Sit, corrupti libero.',
+    teacherAbout: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
   {
     teacherImg: './Teacher pics/Vanessa.jpg',
     teacherName: 'Vanessa Jack',
     teacherExp: 'Software Developer at Microsoft',
-    teacherAbout:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsum eius mollitia? Sit, corrupti libero.',
+    teacherAbout: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
 ];
 
@@ -41,12 +36,14 @@ const speakerBio = document.querySelector('.speakers-cards');
 teachersAll.forEach((speaker) => {
   const div = document.createElement('div');
   div.innerHTML += `
-    <div class="subCard">
-    <img src="${speaker.teacherImg}">
-    <div>
-        <h2>${speaker.teacherName}</h2>
-        <h3>${speaker.teacherExp}</h3>
-        <p>${speaker.teacherAbout}</p>
+    <div class='subCard'>
+    <div class='teachImg-contain'>
+    <img class='teachImg' src="${speaker.teacherImg}">
+    </div>
+    <div class='speaker-desc'>
+        <h2 class='speakh2'>${speaker.teacherName}</h2>
+        <h3 class='speakh3'>${speaker.teacherExp}</h3>
+        <p class='speakp'>${speaker.teacherAbout}</p>
     </div>
     </div>
     `;
